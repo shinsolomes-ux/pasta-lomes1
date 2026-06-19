83969ae998f30c54a86264f99caefa619248749c
shinsolomes-ux <maria.lomes@escola.pr.gov.br>
2026-06-19 08:59:20 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-
+function verificarResposta(substitutaCorreta) {
+    const campoResultado = document.getElementById('resultado');
+    
+    if (substitutaCorreta) {
+        campoResultado.innerText = "🎉 Correto! A irrigação por gotejamento evita o desperdício de água.";
+        campoResultado.style.color = "#2e7d32"; // Texto verde para acerto
+    } else {
+        campoResultado.innerText = "❌ Tente novamente! Essa atitude não ajuda o planeta.";
+        campoResultado.style.color = "#d32f2f"; // Texto vermelho para erro
+    }
+}
